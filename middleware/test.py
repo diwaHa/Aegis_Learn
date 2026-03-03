ee7a8a519d4a9e1aa79b506a02d1ab8b7292a9ea
AjayRajan05 <131186327+AjayRajan05@users.noreply.github.com>
2026-03-03 19:02:35 +0530

    Delete middleware/test.py

--- middleware/test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBTGPfjfo1LkLtfbq5qBhxOD0FiZnsE9uI")
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(f"Model Name: {m.name}")
